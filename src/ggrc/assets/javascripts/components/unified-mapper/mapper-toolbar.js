036eb45a25ab48d919cc702baf993295d2ecaf16
Alexander Novik <al.novik88@gmail.com>
2017-07-19 17:15:32 +0300

    Optimize import of modules

--- src/ggrc/assets/javascripts/components/unified-mapper/mapper-toolbar.js
+++ src/ggrc/assets/javascripts/components/unified-mapper/mapper-toolbar.js
@@ -2,6 +2,8 @@
  Copyright (C) 2017 Google Inc., authors, and contributors
  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
  */
+import './mapper-filter';
+
 (function (can, GGRC, CMS) {
   'use strict';
 
