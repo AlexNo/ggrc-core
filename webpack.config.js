af3edc623940957475d38ac23170b92bb2b80a02
Alexander Novik <al.novik88@gmail.com>
2017-07-25 17:36:06 +0300

    Removed out dated config

--- webpack.config.js
+++ webpack.config.js
@@ -9,23 +9,6 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const _ = require('lodash');
 const path = require('path');
 const ENV = process.env;
-const GGRC = {
-  get_dashboard_modules: function () {
-    return _.compact(_.map(process.env.GGRC_SETTINGS_MODULE.split(' '), function (module) {
-      var name;
-      if (/^ggrc/.test(module)) {
-        name = module.split('.')[0];
-      }
-      if (module === 'development') {
-        name = 'ggrc';
-      }
-      if (!name) {
-        return '';
-      }
-      return './src/' + name + '/assets/assets';
-    }));
-  }
-};
 
 module.exports = {
   entry: {
@@ -36,8 +19,7 @@ module.exports = {
     import: 'ggrc/import',
     export: 'ggrc/export',
     workflow: 'ggrc/workflow',
-    dashboard: 'ggrc/dashboard',
-    styles: GGRC.get_dashboard_modules()
+    dashboard: 'ggrc/dashboard'
   },
   output: {
     filename: isProduction() ? '[name].[chunkhash].js' : '[name]_.js',
